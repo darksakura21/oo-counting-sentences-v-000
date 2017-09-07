4cbb152d8930c3f60f259fb3e633c39bb5abdc0f
Ruth Mesfun <ruth@flatironschool.com>
2017-09-07 22:25:21 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,7 @@ class String
   def count_sentences
     self.include?(".") ? sentence_count = self.split(".").length : sentence_count = 0
     self.include?("!") ? exclamation_count = self.split("!").length - 1 : exclamation_count = 0
-    self.include?("?") ? question_count = self.split("?").length - 1: question_count = 0
+    self.include?("?") ? question_count = self.split("?").length - 2: question_count = 0
 
     total = sentence_count + exclamation_count + question_count
 
